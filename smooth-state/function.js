ddddebf6b8dedc29cf82d9f6ada78cefc802ed00
gignar <oliverjamesphillips@gmail.com>
2016-03-21 15:43:20 +0000

    Update deprecated functions

--- smooth-state/function.js
+++ smooth-state/function.js
@@ -1,20 +1,30 @@
-;(function($) {
-'use strict';
-var $body = $('html, body'),
-    content = $('#main').smoothState({
-      // Runs when a link has been activated
-      onStart: {
-        duration: 1000, // Duration of our animation
-        render: function (url, $container) {
-          // toggleAnimationClass() is a public method
-          // for restarting css animations with a class
-          content.toggleAnimationClass('is-exiting');
-          // Scroll user to the top
-          $body.animate({
-            scrollTop: 0
-          });
+$(function(){
+  'use strict';
+  var $page = $('#main'),
+      options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 2,
+        forms: 'form',
+        onStart: {
+          duration: 1000, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $container.addClass('is-exiting');
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onReady: {
+          duration: 0,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $container.removeClass('is-exiting');
+            // Inject the new content
+            $container.html($newContent);
+          }
         }
-      }
-    }).data('smoothState');
-    //.data('smoothState') makes public methods available
-})(jQuery);
+      },
+      smoothState = $page.smoothState(options).data('smoothState');
+
+});
